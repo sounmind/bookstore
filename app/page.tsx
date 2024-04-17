1998d2e19694558663b1ed7212a9847e2d006ff4
sounmind <devleesche@gmail.com>
2024-04-17 13:07:34 -0400

    feat: main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import React from "react";
+import { BookAddButton } from "../components/BookAddButton";
+import { BookList } from "../components/BookList";
+
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <main>
+      <h1>Bookstore</h1>
+
+      <BookAddButton />
+      <BookList />
+    </main>
+  );
 }
