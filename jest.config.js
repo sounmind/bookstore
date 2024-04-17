be8047a7108eb084d81bc28c9ea5d22a23e9ae2c
sounmind <devleesche@gmail.com>
2024-04-17 11:57:34 -0400

    ci: test env setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest"],
+    "^.+\\.(js|jsx)$": ["ts-jest"],
+  },
+};
